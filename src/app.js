5fa942fcb172048b24bdfa93f2dda4b4f2b98971
mlb406 <mlb406@gmail.com>
2015-05-31 14:46:46 +0100

    Finished release
    Version 1.0

--- src/app.js
+++ src/app.js
@@ -28,51 +28,68 @@ var circ = new UI.Circle({
   backgroundColor: 'white'
 });
 
+var minBar = new UI.Rect({
+  position: new Vector2(0,0),
+  size: new Vector2(144,12),
+  backgroundColor: 'white'
+});
+var hourBar = new UI.Rect({
+  position: new Vector2(0,156),
+  size: new Vector2(144,12),
+  backgroundColor: 'white'
+});
 // Add the elements to the mainFace window, with specific ordering
+mainFace.add(minBar);
+mainFace.add(hourBar);
 mainFace.add(circ);
 mainFace.add(theTime);
 
 // Display window to user
 mainFace.show();
 
-setInterval(updateMins,1000);
-setInterval(updateHours,1000);
+// Create the "hands" to indicate minutes and hours
+var minHand = new UI.Rect({color: 'black', backgroundColor: 'black'});
+var hourHand = new UI.Rect({color:'black', backgroundColor: 'black'});
+
+// Getting initial hand position with the Date() object 
+var mins1 = e.getMinutes();
+var pos2 = Math.round(mins1*2.4);
+var hrs1 = e.getHours();
+var pos3 = hrs1*6;
+minHand.position(new Vector2(pos2,0));
+minHand.size(new Vector2(2,12));
+hourHand.position(new Vector2(pos3,156));
+hourHand.size(new Vector2(2,12));
+
+// Add the hands to the display
+mainFace.add(hourHand);
+mainFace.add(minHand);
+
+// Set the interval for updating clock hands
+setInterval(updateMins,30000); // 30 seconds
+setInterval(updateHours,300000); // 5 minutes
+
+
+// Functions for updating hand position based on time
 
 // Minute hand
 function updateMins() {
   var d = new Date();
   var mins = d.getMinutes();
   var pos1 = Math.round(mins*2.4);
-
-  var minHand = new UI.Rect({
-    position: new Vector2(pos1,0),
-    size: new Vector2(2,12),
-    color: 'white',
-    backgroundColor: 'white'
-  });
-  setInterval(removeMins,1000);
-  function removeMins() {
-    mainFace.remove(minHand);
-  }
-  mainFace.add(minHand);
+  
+  minHand.position(new Vector2(pos1,0));
 }
+
+// Hour hand
 function updateHours() {
   var d = new Date();
   var hours = d.getHours();
-  var pos2 = Math.round(hours*6);
-
-  var hourHand = new UI.Rect({
-    position: new Vector2(pos2,156),
-    size: new Vector2(2,12),
-    color: 'white',
-    backgroundColor: 'white'
-  });
-  setInterval(removeHours,1000);
-  function removeHours() {
-    mainFace.remove(hourHand);
-  }
-  mainFace.add(hourHand);
+  var pos1 = hours*6;
+  
+  hourHand.position(new Vector2(pos1,156));
 }
+
 var f = new Date();
 var y = f.getTime();
 var z1 = x-y;
