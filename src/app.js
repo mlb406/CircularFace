0175429adf3406d4c8729e82a7aab6cf9249417f
mlb406 <mlb406@gmail.com>
2015-07-23 17:48:57 +0100

    Updated with config

--- src/app.js
+++ src/app.js
@@ -1,12 +1,54 @@
 var e = new Date();
 var x = e.getTime();
-// Add analogue hand with Date() obj times by whatever, put that in the Vector2 for position
-// 144/60 = 2.4 (for the minute hand)
-// Import required modules
 var UI = require('ui');
 var Vector2 = require('vector2');
+var Settings = require('settings');
+var Accel = require('ui/accel');
+
+Accel.init();
 
 console.log('Ready!');
+
+var topAnaOn = localStorage.getItem(1);
+var topAna;
+if (topAnaOn !== null) {
+  if (topAnaOn == 'true') {
+    topAna = 'true';
+  } else {
+    topAna = 'false';
+  }
+} else {
+  topAna = 'false';
+}
+
+var botAnaOn = localStorage.getItem(2);
+var botAna;
+if (botAnaOn !== null) {
+  if (botAnaOn == 'true') {
+    botAna = 'true';
+  } else {
+    botAna = 'false';
+  }
+} else {
+  botAna = 'false';
+}
+
+Settings.config(
+  { url: 'https://mlb406.github.io/CircularFace' },
+  function(e) {
+    console.log('Config opened!');
+
+  },
+  function(e) {
+    console.log('Settings recieved!');
+    console.log(JSON.stringify(e.options));
+    var options = e.options;
+    localStorage.setItem(1, (options.bars.top).toString());
+    localStorage.setItem(2, (options.bars.bottom).toString());
+    localStorage.setItem(3, (options.date.on).toString());
+    localStorage.setItem(4, options.date.format);
+  }
+);
 
 // Create the window
 var mainFace = new UI.Window({ fullscreen: true });
@@ -39,8 +81,7 @@ var hourBar = new UI.Rect({
   backgroundColor: 'white'
 });
 // Add the elements to the mainFace window, with specific ordering
-mainFace.add(minBar);
-mainFace.add(hourBar);
+
 mainFace.add(circ);
 mainFace.add(theTime);
 
@@ -61,15 +102,6 @@ minHand.size(new Vector2(2,12));
 hourHand.position(new Vector2(pos3,156));
 hourHand.size(new Vector2(2,12));
 
-// Add the hands to the display
-mainFace.add(hourHand);
-mainFace.add(minHand);
-
-// Set the interval for updating clock hands
-setInterval(updateMins,30000); // 30 seconds
-setInterval(updateHours,300000); // 5 minutes
-
-
 // Functions for updating hand position based on time
 
 // Minute hand
@@ -88,6 +120,53 @@ function updateHours() {
   var pos1 = hours*6;
   
   hourHand.position(new Vector2(pos1,156));
+}
+
+mainFace.on('accelTap', function(e) {
+  var dateSetting = localStorage.getItem(3);
+  if (dateSetting == "true") {
+    var dateFormat = localStorage.getItem(4);
+    var date_format;
+    if (dateFormat == "DD-MM-YY") {
+      date_format = "%d-%m-%y";
+    } else if (dateFormat == "MM-DD-YY") {
+      date_format = "%m-%d-%y";
+    }
+    var dateWind = new UI.Window({ fullscreen: true });
+    var dateText = new UI.TimeText({
+      position: new Vector2(0,0),
+      size: new Vector2(144,168),
+      font: 'bitham-30-black',
+      text: date_format,
+      textAlign: 'center',
+      textOverflow: 'wrap'
+    });
+    dateWind.add(dateText);
+    dateWind.show();
+    setTimeout(function() {
+      dateWind.hide();
+    }, 2000);
+  } else {
+    return;
+  }
+  
+});
+
+//Add handling if top bar is enabled
+if (topAna == 'true') {
+  mainFace.add(minBar);
+  mainFace.add(minHand);
+  setInterval(updateMins,30000); // 30 seconds, top refresh
+} else {
+  return; //Handle if not enabled
+}
+
+if (botAna == 'true') {
+  mainFace.add(hourBar);
+  mainFace.add(hourHand);
+  setInterval(updateHours,300000); // 5 minutes, bottom refresh
+} else {
+  return;
 }
 
 var f = new Date();
